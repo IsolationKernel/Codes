bc64082dfdc324533261c01e3efb7758df368ab6
YE ZHU <19607512+zhuye88@users.noreply.github.com>
2021-09-23 00:43:44 +1000

    update

---
+++ IK/High-dimensional_Evaluation/functions/OHierDP.m
@@ -0,0 +1,78 @@
+function [ Z ] = OHierDP( SimMatrix,dc)
+% DP clustering algorithm with hierarchical outputs
+
+NumIns=size(SimMatrix,2);
+Density=sum(SimMatrix'<=dc);
+
+NeigDisList={};
+NeigDisList{NumIns}=[];
+
+NDL=zeros(1,NumIns);
+
+NeigInsList={};
+NeigInsList{NumIns}=[];
+
+%% link to higher density points
+
+[~,SortDensity]=sort(Density,'descend');
+
+for j=2:NumIns
+    i=SortDensity(j);
+    index=SortDensity(1:j-1);
+    [a, b]=min(SimMatrix(i,index));
+    if ~isempty(a)
+        NeigDisList{i}=a;
+        NeigInsList{i}=index(b);
+        NDL(i)=NeigDisList{i}(1);
+    end
+end
+
+NDL(SortDensity(1))=max(NDL(:));
+%% Normalise Density and NDL
+
+Density=normalize(Density')+0.0000001;
+NDL=normalize(NDL')+0.0000001;
+
+Mult=(Density').*(NDL');
+
+
+%% building tree
+
+Z=[];
+CanList=[];
+
+Nindex=NumIns+1; % point label in tree
+M=max(max(SimMatrix));
+NLabel=1:NumIns;
+scoreList=[Mult; NLabel]'; % combine Mult with point real index
+
+while (~isempty(scoreList))
+    I=scoreList(:,1);
+    [a, ii]=min(I);
+    b=scoreList(ii,2); % find point real index
+    if isempty(NeigInsList{b})
+        CanList=[CanList b];
+        scoreList(ii,:)=[];
+    else
+        Nscore=NeigDisList{b}(1)/M*Density(b);
+        nZ=[NLabel(b) NLabel(NeigInsList{b}(1)) Nscore];
+        NLabel(NLabel==NLabel(b))=Nindex;
+        NLabel(NLabel==NLabel(NeigInsList{b}(1)))=Nindex;
+        Nindex=Nindex+1;
+        scoreList(ii,:)=[];
+        Z=[Z;nZ];
+    end
+end
+
+M=max(Z(:,3));
+
+for i=1:length(CanList)-1
+    nZ=[NLabel(CanList(i)) NLabel(CanList(i+1)) M*1.1];
+    NLabel(NLabel==NLabel(CanList(i)))=Nindex;
+    NLabel(NLabel==NLabel(CanList(i+1)))=Nindex;
+    Nindex=Nindex+1;
+    Z=[Z;nZ];
+end
+
+
+end
--- IK/High-dimensional_Evaluation/functions/testDP.m
+++ IK/High-dimensional_Evaluation/functions/testDP.m
@@ -1,8 +1,6 @@
-function [bf,ba]=testDP(data,class,SimMatrix)
+function [bf,ba] = testDP(data,class,SimMatrix)
 
-%% DP
 
-% SimMatrix=pdist2(data,data,'minkowski',2);
 SimMatrix=SimMatrix./(max(max(SimMatrix)));
 SimMatrix=roundn(SimMatrix,-2);
 range=unique(SimMatrix);
@@ -16,40 +14,20 @@ thre=min(c);
 P=sum(range<thre);
 TT=zeros(20,P);
 NmiScore=[];
-
-
-for i=length(unique(class))
-    parfor j=2:P
-        [ Tclass, ~] = DensityPeak( SimMatrix,range(j),i);
-        if max(Tclass)<200
-             TT(i,j)= fmeasure(class,Tclass); 
-             AA(i,j)= ami(class',Tclass');
-        end       
-    end
+k=length(unique(class));
+AA=[];
+TT=[];
+parfor i=1:P
+    dc=range(i);
+    [ Z ] = OHierDP( SimMatrix,dc);
+    Tclass = cluster(Z,'maxclust',k);
+    TT(i)= fmeasure(class,Tclass);
+    AA(i)= ami(class',Tclass');
+    
 end
-[a,b]=find(TT==max(max(TT)));
-bk =a(end);
-be =range(b(end));
 bf =max(max(TT));
 ba =max(max(AA));
-% %%
-% parfor i=1:P
-%     dc=range(i);
-%     %     A=Mdbscan(data,2,eps,SimMatrix);
-%     %     if max(A)<200
-%     [ Z ] = OHierDP( SimMatrix,dc);
-%     for k=1:20
-%         Tclass = cluster(Z,'maxclust',k);
-%         [~,~,~,NmiScore(i,k),~,~,~,~,~,~] = evaluate(class,Tclass);
-%         para{i,k}=[dc k];
-%         %         end
-%     end
-% end
-% 
-% 
-% [a,b]=find(NmiScore==max(max(NmiScore)));
-% bk =b(end);
-% be =range(a(end));
-% bf =max(max(NmiScore));
-% 
-%  
+
+
+end
+
