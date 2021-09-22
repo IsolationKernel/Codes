8ff36c2685f7a503997be001bf7a67e56d24970a
YE ZHU <19607512+zhuye88@users.noreply.github.com>
2021-09-22 21:38:55 +1000

    update

---
+++ IK/High-dimensional_Evaluation/testIndexing.m
@@ -0,0 +1,17 @@
+
+%% Generating svm file for Brute force vs Ball tree index
+% the best results are shown in Table 2
+
+clear
+load('wGaussians.mat')
+libsvmwrite('Odata.svm', class(1:size(data,1)), sparse(data)); % Original data
+
+tic
+psi=32;
+t=200;
+[ndata] = IKspace (data,data, psi, t);
+toc
+libsvmwrite('IKdata.svm', class(1:size(ndata,1)), sparse(ndata)); % IK features
+
+% once get the transformed data, please run testIndexing.ipynb to conduct the
+% run time comparision 
