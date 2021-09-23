dc060308fb08d0682e94017c9893e3b9f413c472
YE ZHU <19607512+zhuye88@users.noreply.github.com>
2021-09-23 15:37:09 +1000

    update

--- IK/High-dimensional_Evaluation/testIR.m
+++ IK/High-dimensional_Evaluation/testIR.m
@@ -1,19 +1,19 @@
 % the retrieval precision of 5 nearestneighbour as shown in Table 2
 
 clear
-load('ijcnn.mat')
+load('wGaussians.mat')
 k=5;
   
 % data=gpuArray(double(data));
 
-% normalisation
+% normalisation 
  data = (data - min(data)).*((max(data) - min(data)).^-1);
  data(isnan(data)) = 0.5;
  
 %% retrieval using distance
 
 DisMatrix=pdist2(data,data);  
-tic
+ 
 P=[];
 parfor i=1:size(data,1)
     cD=DisMatrix(i,:);
@@ -21,20 +21,20 @@ parfor i=1:size(data,1)
     cC=class(id(2:k+1));
     P(i)=sum(cC-class(i)==0)/k;
 end
-toc
+ 
 Dist_precision=mean(P)
 
 %% retrieval using IK
 
-tic
+ 
 t=200;
 IKprecision=[];
-for ii=1:10
+parfor ii=1:10
     psi=2^ii;    
     [ndata] = SIKspace (data,data, psi, t);     
-    IKDisMatrix=pdist2(full(ndata),full(ndata));  
+    IKDisMatrix=pdist2(ndata,ndata);  
     P=[];    
-    parfor i=1:size(data,1)
+    for i=1:size(data,1)
         cD=IKDisMatrix(i,:);
         [~,id]=sort(cD);
         cC=class(id(2:k+1));
@@ -42,5 +42,5 @@ for ii=1:10
     end
     IKprecision(ii)=mean(P);
 end
-toc
+ 
 BIKprecision=max(IKprecision)
